72a49ef3791372c61b459233e1776221125dedee
石头老师 <shitou1235813@163.com>
2019-10-25 17:28:11 +0800

    Update PCF8591.py
    python3环境下语法格式修改

--- PCF8591.py
+++ PCF8591.py
@@ -48,8 +48,8 @@ def write(val):
 if __name__ == "__main__":
 	setup(0x48)
 	while True:
-		print 'AIN0 = ', read(0)
-		print 'AIN1 = ', read(1)
+		print( 'AIN0 = ', read(0))
+		print( 'AIN1 = ', read(1))
 		tmp = read(0)
 		tmp = tmp*(255-125)/255+125 # LED won't light up below 125, so convert '0-255' to '125-255'
 		write(tmp)
